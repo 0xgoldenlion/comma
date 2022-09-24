faf53e3eb852c77e9704736b98b3777e18610099
0xgoldenlion <0xgoldenlion@gmail.com>
2022-09-24 11:48:48 +0300

    layout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="comma.one">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
