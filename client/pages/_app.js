e17c1a61ac198fb159212a4b8fc521027f7f3df7
0xgoldenlion <0xgoldenlion@gmail.com>
2022-09-24 12:50:38 +0300

    sudoku

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,11 @@
 import Layout from "../components/layout";
 import "../styles/globals.css";
-import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="comma.one">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </PlausibleProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
