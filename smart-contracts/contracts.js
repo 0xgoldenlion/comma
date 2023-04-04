27239241fa06ff6f705f69f4f73a9795de2af934
goldenFlayingLion <100777093+0xgoldenlion@users.noreply.github.com>
2023-04-04 17:06:51 +0300

    Update contracts.js

--- smart-contracts/contracts.js
+++ smart-contracts/contracts.js
@@ -1,5 +1,7 @@
-
-    export const Sudoku = "0x4e1fe63a9D3e908870A34c5B77201B7d299FE3d7"
-    export const Futoshiki = "0x3c7a0F0042292B8c42495423FAc642C99C8f08A3"
-    export const Skyscrapers = "0x5771224B4dF59574fd5a969d3Ed226cb43b2641C"
+     SudokuVerifier = "0xAe467A4CfCe5310C50E2b2A1ad30768A02155fAC"
+     Sudoku = "0x24c23a634dC1dD033Dc2B2063bc689BD35BE610f"
+     FutoshikiVerifier = "0xB11011307e0F3c805387c10aa69F874244b1bec3"
+     Futoshiki = "0x16de95d9199Fceb3546565909eB52a4726B14311"
+     SkyscrapersVerifier = "0x3bBF3EaacC44b3ed11c1713D439aFC5f105871a5"
+     Skyscrapers = "0xa09861c51676cf0fb34b22f8e0199a507d8a26ea"
     
